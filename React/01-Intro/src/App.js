f331a2a91d92856d532ec02743c213fcd684bb6b
Animesh Raj <animeshraj223@gmail.com>
2023-09-12 20:42:16 +0530

    Assignment1

---
+++ React/01-Intro/src/App.js
@@ -0,0 +1,15 @@
+import Books from "./Component/BookList";
+import Books2 from "./Component/BookList2";
+
+function App() {
+  return (
+    <div>
+      <h1>Assignment first part</h1>
+      <Books/>
+      <h1>Assignment second part</h1>
+      <Books2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/01-Intro/src/Component/BookList.js
@@ -0,0 +1,13 @@
+const books = [
+    { title: 'Book 1', author: 'Author 1', year: 2020 },
+    { title: 'Book 2', author: 'Author 2', year: 2018 },
+    { title: 'Book 3', author: 'Author 3', year: 2022 },
+  ];
+
+  function Books(){
+      let data = books.map((book) => <li>{book.title} {book.author} {book.year} </li>);
+
+    return <div> <ul>{data}</ul> </div>
+  }
+
+  export default Books;
